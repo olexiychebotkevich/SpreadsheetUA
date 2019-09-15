0522173e0418d5a07530dd427faaea9629afefea
Олексій Чеботкевич <forest793@ukr.net>
2019-09-15 20:45:16 +0300

    Create new SpreadsheetUA project

---
+++ SpreadsheetUA/SpreadsheetUA/ClientApp/src/components/NavMenu.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+import './NavMenu.css';
+
+export default class NavMenu extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle () {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render () {
+    return (
+        <header>
+            <Navbar color="dark" className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3" dark >
+                <Container>
+                    <NavbarBrand  tag={Link} to="/">SpreadsheetUA</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} className="mr-2" />
+            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={this.state.isOpen} navbar>
+              <ul className="navbar-nav flex-grow">
+                <NavItem>
+                  <NavLink tag={Link} className="text-light" to="/">Home</NavLink>
+                </NavItem>
+              </ul>
+            </Collapse>
+          </Container>
+            </Navbar>
+      </header>
+    );
+  }
+}
